35fb5dd9ae568280f1a0c1355119d066c0536f1b
Sandeep Anand <anandjairam@ufl.edu>
2017-07-02 17:47:43 -0700

    "7-2" - Getting the universal sink and the incident matrix and the matrix product representation

--- Analysis_of_graphs.py
+++ Analysis_of_graphs.py
@@ -9,6 +9,7 @@
     - getConnections() returns all of the vertices in the adjacency list
     - getId() returns the id
     - getWeight() returns the weight of the edge from this vertex to the vertex passed as the parameter
+    - Getting the universal sink and the incident matrix and the matrix product representation
 '''
 
 import numpy as np
@@ -83,25 +84,111 @@ for i in range(8):
 
 print("Printing out the nodes here", g.vertlist.keys())
 
+def issink(M, i, n):
+    for j in range(1,n):
+        if M[i][j] == 1:
+            return False
+        if (M[j][i] == 0 and i!=j):
+            return False
+    return True
+
+def findsink(M,n):
+    i=0
+    j=0
+    print(M)
+    while (i<n and j<n):
+        if M[i][j] == 0:
+            j=j+1
+        elif M[i][j] == 1:
+            i=i+1
+    if (i<n and issink(M,i,n)):
+        print("The sink is at index: ", i)
+    else:
+        print("No universal Sink here")
+
+def incidentmat(B):
+    try:
+        Bt = B.transpose()
+        newval = np.dot(B, Bt)
+        print("product \n", newval)
+    except Exception as e:
+        print(type(e))
+        print(e.args)
+
 def main():
     print("Addign the edges here \n")
-    g.addEdge(0,1,5)
-    g.addEdge(0,6,6)
-    g.addEdge(0,5,2)
-    g.addEdge(1,6,6)
-    g.addEdge(1,2,3)
-    g.addEdge(2,3,5)
-    g.addEdge(3,7,2)
-    g.addEdge(4,3,8)
-    g.addEdge(5,6,2)
-    g.addEdge(5,4,6)
-    g.addEdge(7,6,2)
-    g.addEdge(7,8,4)
-    g.addEdge(8,2,9)
+
+    ch = input("Input a choice of Graph: 1-> random, 2-> directed connected Graph, 3 -> Undirected Graph \n")
+
+    if int(ch) == 1:
+        g.addEdge(0,1,5)
+        g.addEdge(0,6,6)
+        g.addEdge(0,5,2)
+        g.addEdge(1,6,6)
+        g.addEdge(1,2,3)
+        g.addEdge(2,3,5)
+        g.addEdge(3,7,2)
+        g.addEdge(4,3,8)
+        g.addEdge(5,6,2)
+        g.addEdge(5,4,6)
+        g.addEdge(7,6,2)
+        g.addEdge(7,8,4)
+        g.addEdge(8,2,9)
+    elif int(ch) == 2:
+        g.addEdge(0,1,1)
+        g.addEdge(0,3,4)
+        g.addEdge(1,2,2)
+        g.addEdge(2,0,3)
+        g.addEdge(3,2,5)
+
+    gdict = {}
 
     for v in g:
         for w in v.getConnections():
-            print("(%s, %s)" % (v.getId(), w.getId()))
+            if v.getId() in gdict.keys():
+                if len(gdict[v.getId()])!=0:
+                    gdict[v.getId()].append(w.getId())
+                elif len(gdict[v.getId()]) == 0:
+                    gdict[v.getId()] = [w.getId()]
+            else:
+                gdict[v.getId()] = [w.getId()]
+
+    unique = []
+    for key , val in gdict.items():
+        print("key with", key, "has neighbors :", val)
+        if key not in unique:
+            unique.append(key)
+        for x in val:
+            if x not in unique:
+                unique.append(x)
+
+    print("unique items :", unique)
+
+    # Adjacent Matrix representation
+    '''
+    A unique property of list of lists : https://stackoverflow.com/questions/240178/list-of-lists-changes-reflected-across-sublists-unexpectedly
+    '''
+    adjmat = [[0]*len(unique) for x in range(len(unique))]
+
+    for key, val in gdict.items():
+        for item in val:
+            adjmat[key][item] = 1
+
+    print("The universal sink node: ",set(unique - gdict.keys()))
+
+    # Matrix representation using numpy
+    val = np.array(adjmat)
+    print(val)
+    #Find the universal Sink here
+    findsink(val, len(unique))
+
+    #Getting the incident matrix
+    #TODO: Getting hardcoded here, Need to change this
+    B = [[-1,0,1,-1,0],[1,-1,0,0,0],[0,1,-1,0,1],[0,0,0,1,-1]]
+    Bnew = np.array(B)
+    print(Bnew)
+    incidentmat(Bnew)
+
 ## Adding the weights through user input
 if __name__ == '__main__':
     main()
