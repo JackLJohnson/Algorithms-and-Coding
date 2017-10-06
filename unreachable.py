486e546a5734c7fcab70d3e1dc03ee46897aaf76
Sandeep Anand <anandjairam@ufl.edu>
2017-10-05 20:17:27 -0700

    Add files via upload
    - Updated with DFS and BFS

--- unreachable.py
+++ unreachable.py
@@ -3,10 +3,14 @@ You need to print a single integer denoting the number of nodes that are unreach
 Reference :
 -https://docs.python.org/2/library/stdtypes.html#set
 -https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/tutorial/
-
 - Using DFS - Dpeth wise search
+- Using BFS - Breadth first search
 '''
+#TODO: Create a Random graph generator and measure the performance for BFS and DFS
+
+
 import os
+import queue as queue
 path = "./dfs_input.txt"
 graph = {}
 allnodes = set()
@@ -14,14 +18,14 @@ if os.path.exists(path):
     f = open(path,'rb+')
     for idx, line in enumerate(f):
         if idx == 0:
-            N = int(line.split(" ")[0]) #Number of Nodes
-            M = int(line.split(" ")[0]) #Number of Edges
+            N = int(line.strip().decode('UTF-8').split(" ")[0]) #Number of Nodes
+            M = int(line.strip().decode('UTF-8').split(" ")[0]) #Number of Edges
         elif idx == M+1:
-            head = str(line.strip())
+            head = str(line.strip().decode('UTF-8'))
             allnodes.add(head)
         else:
-            a = str(line.split(" ")[0].strip())
-            b = str(line.split(" ")[1].strip())
+            a = str(line.strip().decode('UTF-8').split(" ")[0].strip())
+            b = str(line.strip().decode('UTF-8').split(" ")[1].strip())
             allnodes.add(a)
             allnodes.add(b)
             if a not in graph:
@@ -29,7 +33,7 @@ if os.path.exists(path):
             else:
                 graph[a].append(b)
 
-print "The comlete Graph", graph
+print("The comlete Graph", graph)
 
 def dfs(node, g, visited):
     #Try dfs
@@ -41,9 +45,29 @@ def dfs(node, g, visited):
     return visited
 
 
+def bfs(node, g, visited,q):
+    #try bfs
+    visited.append(node)
+    while not q.empty():
+        q.get()
+        if node in g:
+            for n in g[node]:
+                if n not in visited:
+                    q.put(n)
+                    bfs(n, g, visited, q)
+    return visited
+
+
+
 #print(list(allnodes))
 #print(graph[head])
 #print(allnodes - set(graph[head]))
 
 res = dfs(head,graph,[])
+print("The result of dfs", res)
 print(len(res))
+
+q=queue.Queue()
+q.put(head)
+res2 = bfs(head, graph, [], q)
+print("The result of bfs", res2)
