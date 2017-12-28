dee0752b55670edb59a7700133a4adcc21cfbf01
Sandeep Anand <anandjairam@ufl.edu>
2017-12-28 15:12:24 -0800

    Algoc1pa1

---
+++ karatsuba.py
@@ -0,0 +1,24 @@
+'''
+Karatsuba Multiplication with recurssion
+'''
+class karatsuba(object):
+    """docstring for karatsuba."""
+    def __init__(self):
+        super(karatsuba, self).__init__()
+
+    def karatsubamain(self,n1,n2):
+        if len(str(n1))==1 or len(str(n2))==1:
+            return n1*n2
+        else:
+            mid = int(max(len(str(n1)), len(str(n2)))/2)
+            a,b,c,d = int(str(n1)[:mid]),int(str(n1)[mid:]),int(str(n2)[:mid]),int(str(n2)[mid:])
+            a_c = self.karatsubamain(a,c)
+            b_d = self.karatsubamain(b,d)
+            ad_plus_bc = self.karatsubamain(a+b,c+d) -a_c- b_d
+            prodf = (a_c*10**(2*mid)) + (ad_plus_bc*(10**(mid)))+ b_d
+            return prodf
+
+obj = karatsuba()
+nums1 = 3141592653589793238462643383279502884197169399375105820974944592
+nums2 = 2718281828459045235360287471352662497757247093699959574966967627
+print(obj.karatsubamain(nums1, nums2))
