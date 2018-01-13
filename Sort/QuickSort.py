f2962a3c7f8f5cd2d0fe274e94e69a91573ea64f
Sandeep Anand <anandjairam@ufl.edu>
2018-01-13 10:23:01 -0800

    AlgoC1W3

--- Sort/QuickSort.py
+++ Sort/QuickSort.py
@@ -1,7 +1,15 @@
 '''
 QuickSort
 1. Ans: 162085
-2. Ans:
+2. Ans: 164123
+3. Ans: 138382
+
+first middle final
+if array is of odd length, it is the middle length
+if array of length 2k use the kth element
+EXAMPLE:
+For the input array 8 2 4 5 7 1 you would consider the first (8), middle (4), and last (1) elements;
+since 4 is the median of the set {1,4,8}, you would use 4 as your pivot element
 '''
 import urllib.request as url1
 import os
@@ -30,7 +38,7 @@ class Quicksort(object):
 
     def qsort(self, arr, l, r):
         if l<r:
-            q=self.partition(arr,l,r)
+            q=self.partitionrand(arr,l,r)
             self.qsort(arr,l,q-1)
             self.qsort(arr,q+1,r)
 
@@ -48,9 +56,21 @@ class Quicksort(object):
         print(self.comp)
         return i-1
 
+    def getpivot(self,arr,l,r):
+        k=len(arr)
+        m=0 if (r-l)==1 else int((r-l)/2)+l
+        f,l,mid=arr[l],arr[r],arr[m]
+        #else:f,l,mid=arr[l],arr[r],arr[int((r-l)/2)-1]
+        res=[f,mid,l]
+        idx=res.index(sorted(res)[1])
+        return idx
+
+    def getpivotmod(self,arr,l,r):
+        m=0 if (r-l)==1 else int((r-l)/2)+l
+        M,L,R=arr[m],arr[l],arr[r]
+        return sorted([(L,l), (M,m), (R,r)])[1][1]
+
     def partition_r(self,arr,l,r):
-        #self.comp+=len(arr)-1
-        #print("comp",self.comp)
         i,p=l-1,arr[r]
         for j in range(l,r):
             self.comp+=1
@@ -62,12 +82,31 @@ class Quicksort(object):
         return i+1
 
     def partition2(self,arr,l,r):
-        #self.comp+=len(arr)-1
-        #print("comp",self.comp)
+        arr[l],arr[r]=arr[r],arr[l]
         i,p=l+1,arr[l]
         for j in range(l+1,r+1):
             self.comp+=1
-            if arr[j]<p:
+            if arr[j]<=p:
+                arr[i],arr[j]=arr[j],arr[i]
+                i+=1
+        arr[l],arr[i-1]=arr[i-1],arr[l]
+        print(self.comp)
+        return i-1
+
+    def partitionrand(self,arr,l,r):
+        idx=self.getpivotmod(arr,l,r)
+        p=arr[idx]
+        arr[l],arr[idx]=arr[idx],arr[l]
+        #print(pval,idx)
+        # if idx==2:arr[l],arr[r]=arr[r],arr[l]
+        # if idx==1:
+        #     length=len(arr)
+        #     if length%2!=0:arr[l],arr[int(length/2)]=arr[int(length/2)],arr[l]
+        #     else: arr[l],arr[int(length/2)-1]=arr[int(length/2)-1],arr[l]
+        i=l+1
+        for j in range(l+1,r+1):
+            self.comp+=1
+            if arr[j]<=p:
                 arr[i],arr[j]=arr[j],arr[i]
                 i+=1
         arr[l],arr[i-1]=arr[i-1],arr[l]
@@ -78,8 +117,8 @@ if __name__=="__main__":
     filename = "C:/Public/Code/testcases/QuickSort.txt"
     obj=Quicksort()
     if os.path.isfile(filename):
-        #res=obj.processdata(filename)
-        res=[3,9,8,4,6,10,2,5,7,1]
+        res=obj.processdata(filename)
+        #res=[13,9,8,15,4,6,10,2,5,7,14,12,11,1,3]
         #print(res[0:10],len(res))
         start=time.time()
         print(obj.qsort(res,0,len(res)-1))
